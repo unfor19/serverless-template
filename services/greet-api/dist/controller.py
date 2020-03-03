48a35adfc6a02726f1f4bd145a3ea26848a2e2d3
Meir Gabay <unfor19@gmail.com>
2020-03-04 00:16:48 +0200

    MOD - removed dist output

--- services/greet-api/dist/controller.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-def prepareResponse(body, status_code):
-    return {
-        'body': json.dumps(body),
-        'statusCode': status_code,
-        'isBase64Encoded': False
-    }
