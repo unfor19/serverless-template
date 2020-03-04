3c89541b41c736aab84439242b0fc26d0bc3d9bd
Meir Gabay <unfor19@gmail.com>
2020-03-04 03:28:28 +0200

    ADD - docker push script

---
+++ scripts/docker_push.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+PROJECT_DIR="${PWD}"
+
+error (){
+    local msg=$1
+    echo -e "\033[31mFATA[0000]\e[0m ${msg}"
+    usage
+    exit
+}
+
+usage(){
+  cat << EOF
+Usage: bash ./scripts/docker_build.sh -tv v2
+Short  | Full             | Default
+-tv    | --tag_version    | Required
+-h     | --help
+EOF
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -tv | --tag_version )
+            shift
+            tag_version=$1
+        ;;                                                       
+        -h | --help ) usage
+            exit
+        ;;
+        * )          usage
+            exit 1
+    esac
+    shift
+done
+
+[[ -z ${tag_version} ]] && error "Tag version name is required"
+
+image_name="unfor19/serverless-template"
+docker login && docker push "${image_name}:${tag_version}"
