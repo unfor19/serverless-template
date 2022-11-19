11c7632855fe97ab396bed51519073fa7faac727
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 14:57:27 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/dynamodump.py
+++ scripts/dynamodump.py
@@ -231,7 +231,26 @@ def do_get_s3_archive(profile, region, bucket, table, archive):
         try:
             logging.info("Extracting tar file...")
             with tarfile.open(name=output_file, mode="r:bz2") as a:
-                a.extractall(path=".")
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(a, path=".")
         except tarfile.ReadError as e:
             logging.exception("Error reading downloaded archive\n\n" + str(e))
             sys.exit(1)
